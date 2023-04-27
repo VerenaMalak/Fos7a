8a19e6deee487fe1380264eeeb5b18ea7b4f1a24
VerenaMalak <127668837+VerenaMalak@users.noreply.github.com>
2023-04-27 15:25:38 +0200

    more images
    new button

--- place_information.php
+++ place_information.php
@@ -93,8 +93,8 @@ if (isset($_POST['place_information'])){
 
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <label class="formFileMultiple">Place Images<span class="text-danger">*</span></label>
-                                                <input name="placeimages" id="placeimages" type="file" class="form-control" multiple >
+                                            <label class="formFileMultiple">Add your main image:<span class="text-danger">*</span></label>
+                                                <input name="placeimages" id="placeimages" type="file" class="form-control" >
                                         </div> 
                                     </div><!--end col-->
 
@@ -105,6 +105,43 @@ if (isset($_POST['place_information'])){
                                         </div> 
                                     </div><!--end col-->
                                     
+
+                                    <div class="col-md-6">
+                                        <div class="mb-3">
+                                        <br>
+                                             <!-- Button trigger modal -->
+                                         <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                           For putting more images click:
+                                            </button>
+
+                                        <!-- Modal -->
+                                      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                                 <h1 class="modal-title fs-5" id="exampleModalLabel">Add maximum 5 images</h1>
+                                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                            <div class="modal-body">
+                                        <!--el tables ely gwa-->
+                                    
+                                             <input name="fiveimages" id="fiveimages" type="file" class="form-control" multiple >   
+                                                </div>
+                                                <!--model footer-->
+                                       <div class="modal-footer">
+                                        <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
+                                     <button type="button" class="btn btn-primary">Submit</button>
+                                         </div>
+                                            </div>
+                                             </div>
+                                          </div> 
+
+                                        </div> 
+                                    </div><!--end col-->
+
+                                   
+
+                                
                                     <div class="row">
                                       <div class="col-12">
                                       <div class="d-grid">
